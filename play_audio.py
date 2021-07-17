97b8f6fc7b6daa1af95536f2888b2817a4421907
Jordan Masirika <jordan.mas33@gmail.com>
2021-07-18 00:44:51 +0200

    Forgot to commit. Commit after mastery of playsound

---
+++ play_audio.py
@@ -0,0 +1,31 @@
+from playsound import playsound
+from gtts import gTTS
+import os
+import time
+
+
+questions = ['Hi there', 'Are you ready to answer the questions? Please reply by yes or no',
+             'Did you use public transport today?', 'Have you had any contact with a COVID-19 patient?',
+             'Do you have COVID-19 symptoms?', 'Are you complying with COVID-19 protocols?',
+             'Thank you for answering all the questions. Have a nice day ahead']
+
+
+def transcribe():
+    i = 0
+    for question in questions:
+        sI = str(i)
+        trans = gTTS(text=question, lang='en', slow=False)
+        if 'trans.mp3' not in os.listdir('sounds'):
+            trans.save('sounds/trans' + sI + '.mp3')
+        else:
+            playsound('sounds/trans' + sI + '.mp3')
+        playsound('sounds/trans' + sI + '.mp3')
+        i = i + 1
+        time.sleep(2)
+
+transcribe()
+
+# trans1 = gTTS(text=q0, lang='en', slow=False)
+# if 'trans1.mp3' not in os.listdir('sounds'):
+#    trans1.save('sounds/trans1.mp3')
+# playsound('sounds/trans1.mp3')
