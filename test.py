70780bc188cda803130b637dd450fc854f558fc9
Jordan Masirika <jordan.mas33@gmail.com>
2021-07-22 19:17:56 +0200

    Deleted test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-
-GOOGLE_CLOUD_SPEECH_CREDENTIALS = json.load(open('google.json'))
-print(GOOGLE_CLOUD_SPEECH_CREDENTIALS)
-print('\n')
-y = json.dumps(GOOGLE_CLOUD_SPEECH_CREDENTIALS)
-
-print(y)
